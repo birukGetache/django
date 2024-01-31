7c4601049c7caf0fc673c1a055767ccbc87128b5
birukGetachew <burab1742@gmail.com>
2024-01-31 13:43:43 -0800

    with admin dashbord

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
 from . import views
 urlpatterns=[
-    path("<int:id>",views.index,name="index"),
+    path("<int:id>",views.index,name="index"),#if we write 788888 it display 788888
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import ToDoList,Item
 def index(response,id):
     ls=ToDoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" %ls.name)#see me id of i write in my website
+    items= ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><h1>%s</h1>" %(ls.name,str(items.text)))#see me id of i write in my website
 
 #this is our first from .views import 
